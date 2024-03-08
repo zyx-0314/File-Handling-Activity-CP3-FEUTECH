4c67c377c0013402f6f68c08c4824046f397c3d9
nyebe-14 <ramirezian037@gmail.com>
2024-03-08 09:13:42 +0800

    Update formative-7-explanation.cpp

--- formative-7-explanation.cpp
+++ formative-7-explanation.cpp
@@ -2,200 +2,227 @@
 #include <string>
 #include <fstream>
 
-// holds data of player
+// Structure that holds Data
 struct Player
 {
-	const static int MAX = 10;
+  // Controls Maximum Scores that can be hold
+  const static int MAX = 10;
 
-	std::string nickname = "";
-	int age = 0;
-	double scores[MAX] = {};
+  std::string nickname = "";
+  int age = 0;
+  double scores[MAX] = {};
 
-	void Display()
-	{
-		std::cout
-			<< nickname << " - " << age << "\n"
-			<< "Scores:\n";
-		for (size_t i = 0; i < this->MAX; i++)
-			std::cout << "\t" << scores[i] << "\n";
-	}
+  // Function used to display data stored
+  void Display()
+  {
+    std::cout
+        << "Name: " << this->nickname << std::endl
+        << "Age: " << this->age << std::endl
+        << "Scores: ";
+    for (size_t i = 0; i < this->MAX; i++)
+    {
+      std::cout << scores[i];
+
+      if (i == this->MAX - 1)
+        std::cout << std::endl;
+      else
+        std::cout << ", ";
+    }
+  }
 };
 
+// Used to create node inside the LinkedList
 struct Node
 {
-	// address
-	Node* next;
+  // Address of Following Node
+  Node *next;
 
-	// data
-	Player player;
+  // Data Node Holds
+  Player player;
 };
 
-void MainMenu(int&);
+// Functions that is used for Menus
+void MainMenu(int &);
 
-void AddData(Player&);
-void AddRecord(Player&, Node*& head, Node*& tail);
-void ViewRecord(Node*&);
-void OpenFile(Node*& head);
-void ReadFile(Node*& head);
-void CloseFile();
+void AddData(Player &);
+void AddRecord(Player &, Node *&head, Node *&tail);
+void ViewRecord(Node *&);
+void OpenFile(Node *&, std::string);
+void ReadFile(std::string);
 
 int main()
 {
-	int choice;
-	// using principle of FILO
-		// it only uses head
-	Node* head = NULL;
-	Node* tail = NULL;
-	Player tempDataHolder;
+  int choice;
+  Player tempDataHolder;
+  std::string recordFile = "Formative-7.txt";
+  Node *head = NULL, *tail = NULL;
 
-	do
-	{
-		MainMenu(choice);
+  do
+  {
+    MainMenu(choice);
 
+    switch (choice)
+    {
+    case 1:
+      AddData(tempDataHolder);
+      AddRecord(tempDataHolder, head, tail);
+      system("cls");
+      break;
+    case 2:
+      ViewRecord(head);
+      break;
+    case 3:
+      OpenFile(head, recordFile);
+      break;
+    case 4:
+      ReadFile(recordFile);
+      break;
+    case 0:
+      exit(0);
+    default:
+      break;
+    }
 
-		switch (choice)
-		{
-		case 1:
-			AddData(tempDataHolder);
-			AddRecord(tempDataHolder, head, tail);
-			system("cls");
-			break;
-		case 2:
-			ViewRecord(head);
-			break;
-		case 3:
-			OpenFile(head);
-			CloseFile();
-			break;
-		case 4:
-			ReadFile(head);
-			CloseFile();
-			break;
-		case 0:
-			exit(0);
-		default:
-			break;
-		}
-
-	} while (true);
-
+  } while (true);
 }
 
-void MainMenu(int& choice)
+void MainMenu(int &choice)
 {
-	do
-	{
-		std::cout
-			<< "1. Add Record\n"
-			<< "2. Check Record\n"
-			<< "3. Save\n"
-			<< "4. Read\n"
-			<< "0. Exit\n"
-			<< "\n"
-			<< ":: ";
-		std::cin >> choice;
-		system("cls");
-	} while (choice < 0 || choice > 4);
+  do
+  {
+    std::cout
+        << "1. Add Record\n"
+        << "2. Check Record\n"
+        << "3. Save\n"
+        << "4. Read\n"
+        << "0. Exit\n"
+        << "\n"
+        << ":: ";
+    std::cin >> choice;
+    system("cls");
+  } while (choice < 0 || choice > 4);
 }
 
-void AddData(Player& tempDataHolder)
+// THis Function add Information inside the Node
+void AddData(Player &tempDataHolder)
 {
-	// enter data nickname and age
-	std::cout << "Enter Player Name: ";
-	std::cin >> tempDataHolder.nickname;
+  // Enter data nickname and age
+  std::cout << "Enter Player Name: ";
+  std::cin >> tempDataHolder.nickname;
 
-	std::cout << "Enter Player Age: ";
-	std::cin >> tempDataHolder.age;
+  std::cout << "Enter Player Age: ";
+  std::cin >> tempDataHolder.age;
 
-	// enter scores, number of scores are based on MAX
-	for (size_t i = 0; i < tempDataHolder.MAX; i++)
-	{
-		std::cout << "Enter Player Scores " << i + 1 << ": ";
-		std::cin >> tempDataHolder.scores[i];
-	}
+  // Enter scores, number of scores are based on MAX
+  for (size_t i = 0; i < tempDataHolder.MAX; i++)
+  {
+    std::cout << "Enter Player Scores " << i + 1 << ": ";
+    std::cin >> tempDataHolder.scores[i];
+  }
 }
 
-void AddRecord(Player& tempDataHolder, Node*& head, Node*& tail)
+// This function adds Node in the Linked List
+void AddRecord(Player &tempDataHolder, Node *&head, Node *&tail)
 {
-	Node* temp = new Node;
-	
+  Node *temp = new Node;
 
-	temp->player = tempDataHolder;
+  // Preparing the node, by storing Data and Setting it next as NULL
+  temp->player = tempDataHolder;
+  temp->next = NULL;
 
-	temp->next = NULL;
+  // If there is no Head(No Linked List yet)
+  if (!head)
+    head = temp;
+  // If there is Head
+  else
+    tail->next = temp;
 
-	if (head == NULL)
-	{
-		head = temp;
-	}
-	else {
-		tail->next = temp;
-	
-	}
-	tail = temp;
+  // Set the new tail
+  tail = temp;
 }
 
-void ViewRecord(Node*& head)
+// Traverse Records
+void ViewRecord(Node *&head)
 {
-	Node* temp = head;
+  // create temporary node(current) for traversing the linked list
+  Node *temp = head;
 
-	if (!head)
-		std::cout << "-- No List Yet --";
-	else
-	{
-		while (temp)
-		{
-			temp->player.Display();
-			temp = temp->next;
-		}
-	}
+  if (!head)
+    std::cout << "-- No List Yet --";
+  else
+  {
+    // traverse while there is data in the linked list
+    while (temp)
+    {
+      // call the display function of the player structure
+      temp->player.Display();
+      // Move to the next node
+      temp = temp->next;
+    }
+  }
 
-	system("pause");
-	system("cls");
+  system("pause");
+  system("cls");
 }
 
-void OpenFile(Node*& head) {
-	std::ofstream outFile("player_recordz.txt");
-	Node* current = head;
-
-	if (current != nullptr && outFile.is_open()) {
-
-		while (current)
-		{
-			outFile << "Name: " << current->player.nickname << std::endl;
-			outFile << "Age: " << current->player.age << std::endl;
-			outFile << "Scores: ";
-			for (int i = 0; i < head->player.MAX; ++i) {
-				outFile << current->player.scores[i] << " ";
-			}
-			outFile << std::endl;
-			current = current->next;
-		}
-
-		std::cout << "Records saved to 'player_recordz.txt'." << std::endl;
-	}
-	else {
-		std::cout << "Invalid" << std::endl;
-	}
-	
-	outFile.close();
-	
-	
-}
-
-void ReadFile(Node*& head)
+// Create Permanent File and Stores Data hold in the LinkedList
+void OpenFile(Node *&head, std::string recordFile)
 {
-	std::ifstream inFile("player_recordz.txt");
+  // create temporary node(current) for traversing the linked list
+  Node *current = head;
+  // creates or connect to file
+  std::ofstream outFile(recordFile);
 
-	std::cout << inFile.rdbuf();
+  // checks if the file is open and if there is data in the linked list
+  if (current != nullptr && outFile.is_open())
+  {
+    // traverse while there is data in the linked list
+    while (current)
+    {
+      outFile
+          << "Name: " << current->player.nickname << std::endl
+          << "Age: " << current->player.age << std::endl
+          << "Scores: ";
 
-	inFile.close();
+      for (int i = 0; i < head->player.MAX; ++i)
+      {
+        outFile << current->player.scores[i];
+        if (i == current->player.MAX - 1)
+          outFile << std::endl;
+        else
+          outFile << ", ";
+      }
 
-	system("pause");
-	system("cls");
+      // Move to the next node
+      current = current->next;
+    }
+
+    std::cout << "Records saved to 'player_recordz.txt'." << std::endl;
+  }
+  else
+    std::cout << "Invalid" << std::endl;
+
+  outFile.close();
+  system("pause");
+  system("cls");
 }
 
-// Function to indicate closing the file
-void CloseFile() {
-	std::cout << "File closed." << std::endl;
+// Read Permanent File
+void ReadFile(std::string recordFile)
+{
+  // Read from recordFile
+  std::ifstream inFile(recordFile);
+
+  // Check if file is open
+  if (!inFile.is_open())
+    std::cout << "File not found." << std::endl;
+  else
+    // Convert Data
+    std::cout << inFile.rdbuf();
+
+  // Close file connection
+  inFile.close();
+
+  system("pause");
+  system("cls");
 }
